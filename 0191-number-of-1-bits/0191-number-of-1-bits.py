457b673a433cde2ef42167064d960a1b5fe18769
TalhaHKhan <96955565+Talha-Here@users.noreply.github.com>
2024-11-08 02:35:02 +0500

    Time: 0 ms (100%), Space: 16.5 MB (50.71%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -0,0 +1,20 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        # ============== sol 1 --> using mod (%) and right shift by 1 ==============
+        # res = 0
+        # while n: # O(32)
+        #     if n % 2 == 1: 
+        #         res += 1
+        #     # right shift
+        #     n = n >> 1
+        # return res
+# T.C & S.C = O(1)
+
+    # ============== sol 2 --> using n = n & (n-1) ==============
+    # helps in saving time by removing zeros. e.g 10000001, in this we will skip zeros
+
+        res = 0 
+        while n:
+            n = n & (n-1)
+            res += 1
+        return res
