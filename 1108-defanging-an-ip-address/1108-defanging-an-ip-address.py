a03e967ccb097012a64ec9a0074cb3b276300310
TalhaHKhan <96955565+Talha-Here@users.noreply.github.com>
2025-09-05 01:30:01 +0500

    Time: 42 ms (23.19%), Space: 17.7 MB (61.89%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -1,10 +1,22 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        res = ""
+
+        #  ---- This is O(n^2) cuz, stirngs are immutable 
+        # res = ""
+        # for elem in address:
+        #     if elem == ".":
+        #         res += "[.]" # O(n)
+        #     else:
+        #         res += elem # O(n)
+        # return res
+
+        # this is O(n) as ARRAY which is mutable and has O(1) inserts
+        res = []
         for elem in address:
             if elem == ".":
-                res += "[.]"
+                res.append("[.]") # O(1)
             else:
-                res += elem
-        return res
+                res.append(elem) # O(1)
+        return "".join(res) # O(n)
+        
         
